26ba9ef41df1d20d23bb45ccdfbc27c517ab8182
cedchua <cedchuaa0324@gmail.com>
2024-05-11 23:37:35 +0800

    dplymnt 5_11_24

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
-axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "https://mdrbakingsupplies.com/"; // real
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem('auth_token');
   config.headers.Authorization = token ? `Bearer ${token}` : '';
