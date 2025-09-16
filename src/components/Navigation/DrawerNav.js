d752998ae4df78284782c6ef2b46c69e878d3b62
ced <cedchuaa0324@gmail.com>
2025-09-17 02:16:29 +0800

    09/17/25

--- src/components/Navigation/DrawerNav.js
+++ src/components/Navigation/DrawerNav.js
@@ -1632,7 +1632,7 @@ export default function PersistentDrawerLeft() {
                         </ListItemButton>
 
                         <Collapse in={openCategoryReport} timeout="auto" unmountOnExit>
-                            {stockReportList.map((nav, index) => (
+                            {categoryReportList.map((nav, index) => (
                                 <ListItem key={nav.name} component={Link} href={nav.url} disablePadding>
                                     <ListItemButton sx={{ pl: 6 }}>
                                         <ListItemIcon>
