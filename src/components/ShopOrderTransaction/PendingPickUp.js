45bcce8b9ba882ec03a0c7b4e59e6f00010b5c71
ced <cedchuaa0324@gmail.com>
2026-01-26 22:57:10 +0800

    1/16/25

--- src/components/ShopOrderTransaction/PendingPickUp.js
+++ src/components/ShopOrderTransaction/PendingPickUp.js
@@ -35,6 +35,8 @@ const PendingPickUp = () => {
         status: 2
     });
 
+    const [role, setRole] = useState(localStorage.getItem('role_as'));
+
     const [formDeliveryErrors, setFormDeliveryErrors] = useState({});
     const [formErrorsPickUp, setFormErrorsPickup] = useState({});
     const [isDeliveryDisabled, setIsDeliveryDisabled] = useState(false);
@@ -450,6 +452,12 @@ const PendingPickUp = () => {
         '& .MuiTextField-root': { m: 1, width: '25ch' },
     };
 
+    const numberFormat = (value) =>
+        new Intl.NumberFormat('en-us', {
+            style: 'currency',
+            currency: 'PHP'
+        }).format(value).replace(/(\.|,)00$/g, '');
+
 
 
     return (
@@ -508,7 +516,6 @@ const PendingPickUp = () => {
                                 <th>For Trucking</th>
                                 <th>Rider</th>
                                 <th >Pick Up Status</th>
-                                <th>Update Date</th>
                                 <th></th>
                                 <th></th>
                                 <th></th>
@@ -529,8 +536,21 @@ const PendingPickUp = () => {
                                         <td>{shopOrderTransaction.total_cash}</td>
                                         <td>{shopOrderTransaction.total_online}</td>
                                         <td style={{ fontWeight: 'bold', }}>{shopOrderTransaction.shop_order_transaction_total_price}</td>
-                                        <td style={{ fontWeight: 'bold', }}>{shopOrderTransaction.profit}</td>
-                                        <td>{shopOrderTransaction.date}</td>
+                                        {
+                                            role == 2 && (
+                                                <td style={{ fontWeight: 'bold', }}>{shopOrderTransaction.profit != 0 ? numberFormat(shopOrderTransaction.profit) : ""}
+                                                </td>
+                                            )
+                                        }
+
+                                        <td>{shopOrderTransaction.date != shopOrderTransaction.created_at ? <p style={{ fontWeight: 'bold', color: 'orange', }}>{shopOrderTransaction.date}</p>
+                                            : shopOrderTransaction.date}
+                                            {shopOrderTransaction.status == 2 && shopOrderTransaction.is_pickup == 0 ?
+                                                <IconButton>
+                                                    <UpdateIcon color="primary" onClick={(e) => handleOpen(shopOrderTransaction.id, e)} />
+                                                </IconButton> : ""
+                                            }
+                                        </td>
                                         <td>{shopOrderTransaction.status === 1 ? <p style={{ fontWeight: 'bold', color: 'green', }}>COMPLETED</p>
                                             : shopOrderTransaction.status === 2 ? <p style={{ fontWeight: 'bold', color: 'orange', }}>PENDING</p> :
                                                 <p style={{ fontWeight: 'bold', color: 'red', }}>CANCELLED</p>}
@@ -560,11 +580,6 @@ const PendingPickUp = () => {
                                                 <p style={{ fontWeight: 'bold', color: 'orange', }}>WAITING</p>}</p>
                                             <IconButton>
                                                 <UpdateIcon color="primary" onClick={(e) => handleOpenPickUp(shopOrderTransaction.id, e)} />
-                                            </IconButton>
-                                        </td>
-                                        <td>
-                                            <IconButton>
-                                                <UpdateIcon color="primary" onClick={(e) => handleOpen(shopOrderTransaction.id, e)} />
                                             </IconButton>
                                         </td>
                                         <td>
