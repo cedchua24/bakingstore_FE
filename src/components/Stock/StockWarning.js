3349b9f629eb4c1e6d1e09fcda26d4d11fdcaac9
ced <cedchuaa0324@gmail.com>
2026-01-29 15:19:48 +0800

    1/29/26

--- src/components/Stock/StockWarning.js
+++ src/components/Stock/StockWarning.js
@@ -254,8 +254,8 @@ const StockWarning = (props) => {
                                         <td>{product.stock_pc < product.stock_warning ? <p style={{ fontWeight: 'bold', color: 'red', }}>{product.stock_pc}</p>
                                             : <p >{product.stock_pc}</p>}
                                         </td>
-                                    </>}
-                                {/* <td>{product.weight}x{product.quantity}kg</td> */}
+                                    </>
+                                }
                                 <td>{product.quantity === 1 ? <p >{product.weight}kg</p>
                                     : <p >{product.quantity}x{Number.isInteger(product.weight / product.quantity) ? (product.weight / product.quantity) : (product.weight / product.quantity).toPrecision(2)}{product.variation}</p>}
                                 </td>
